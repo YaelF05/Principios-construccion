8894aca1aedce720be1000e5abaee77e8b2ef95f
FRANCO TOLEDO YAEL <zS23017346@estudiantes.uv.mx>
2025-03-04 09:43:40 -0600

    actividad

--- FizzBuzz/src/ConsoleBasedFizzBuzz.java
+++ FizzBuzz/src/ConsoleBasedFizzBuzz.java
@@ -1,18 +1,18 @@
 public class ConsoleBasedFizzBuzz implements FizzBuzz {
 
     @Override
-    public void print(int num, int num2) {
-        while (num < num2) {
-            if (num % 3 == 0 && num % 5 == 0) {
+    public void print(int from, int to) {
+        while (from < to) {
+            if (from % 3 == 0 && from % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (num % 3 == 0) {
+            } else if (from % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (num % 5 == 0) {
+            } else if (from % 5 == 0) {
                 System.out.println("Buzz");
             } else {
-                System.out.println(num);
+                System.out.println(from);
             }
-            num++;
+            from++;
         }
     }
 }
