72cf4c3a4ac6cde5e0540fbbfbdd9dda416e8b6d
YaelF05 <yaelfrnc@gmail.com>
2025-03-09 10:25:28 -0600

    prueba

--- cleanCode/src/Main.java
+++ cleanCode/src/Main.java
@@ -4,8 +4,8 @@ import personal.*;
 public class Main {
     public static void main(String[] args) {
         Masajista masajista1 = new Masajista(1, "Pepe", "Torres", 32, "Terapeuta", 7);
-        Futbolista futbolista1 = new Futbolista(2, "Pedro","Ruiz",24,10,"Defensa");
-        Entrenador entrenador1 = new Entrenador(3,"Jose","Bustamante",38,143);
+        Futbolista futbolista1 = new Futbolista(2, "Pedro", "Ruiz", 24, 10, "Defensa");
+        Entrenador entrenador1 = new Entrenador(3, "Jose", "Bustamante", 38, 143);
 
         masajista1.concentrarse();
         futbolista1.jugarPartido();
